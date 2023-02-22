c059988bdb830508e8ef027f9f45dc942f87a8a8
Simon Engelhardt <se@inga-hameln.de>
2023-02-22 10:51:20 +0100

    delete plant from list

--- src/app/components/plant-list/plant-list.component.ts
+++ src/app/components/plant-list/plant-list.component.ts
@@ -2,17 +2,18 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Plant, PlantService } from 'src/app/services/plant.service';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-plant-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './plant-list.component.html',
 })
 export class PlantListComponent {
   plants$: Observable<Plant[]>;
 
-  constructor(plantService: PlantService) {
+  constructor(public plantService: PlantService) {
     this.plants$ = plantService.getPlants$();
   }
 }
