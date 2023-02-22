c4c9b1d01aa274831b3408e8dd37e94d673f0af4
Simon Engelhardt <se@inga-hameln.de>
2023-02-22 10:40:04 +0100

    show existing plants in a list

--- src/app/components/plants/plants.component.ts
+++ src/app/components/plants/plants.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ButtonComponent } from '../button/button.component';
 import { LogoutComponent } from '../logout/logout.component';
+import { PlantListComponent } from '../plant-list/plant-list.component';
 
 @Component({
   selector: 'app-plants',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, ButtonComponent, LogoutComponent],
+  imports: [CommonModule, RouterLink, RouterOutlet, ButtonComponent, LogoutComponent, PlantListComponent],
   templateUrl: './plants.component.html',
   styleUrls: ['./plants.component.scss'],
 })
